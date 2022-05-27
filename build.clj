81d75d5cda2813d99414e0d17be2b455f2b79f76
niten <niten@fudo.org>
2022-05-27 14:33:19 -0700

    Try again

--- build.clj
+++ build.clj
@@ -2,45 +2,54 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib-name 'org.fudo/fudo-clojure)
-(def major-version "0")
 
 (def target-dir "./target")
 (def class-dir (format "%s/classes" target-dir))
-(def version (format "%s.%s" major-version (b/git-count-revs nil)))
 (def basis (b/create-basis {:project "deps.edn"}))
-(def jar-file (format "%s/%s-%s.jar"
-                      target-dir (name lib-name) version))
-(def uber-file (format "%s/%s-%s-standalone.jar"
-                       target-dir (name lib-name) version))
 
-(defn clean [_]
+(def timestamp (-> (java.time.LocalDateTime/now)
+                   (.format java.time.format.DateTimeFormatter/BASIC_ISO_DATE)))
+
+(defn jar-filename [version]
+  (format "%s/%s-%s.jar"
+          target-dir (name lib-name) version))
+(defn uber-filename [version]
+  (format "%s/%s-%s-standalone.jar"
+          target-dir (name lib-name) version))
+
+(defn clean [params]
   (println (format "removing build target folder \"%s\"" target-dir))
-  (b/delete {:path target-dir}))
+  (b/delete {:path target-dir})
+  params)
 
-(defn jar [_]
-  (clean nil)
-  (b/copy-dir {:src-dirs   ["src"]
-               :target-dir target-dir})
-  (b/compile-clj {:basis     basis
-                  :src-dirs  ["src"]
-                  :class-dir class-dir})
-  (b/write-pom {:class-dir class-dir
-                :lib       lib-name
-                :version   version
-                :basis     basis
-                :src-dirs  ["src"]})
-  (b/jar {:class-dir class-dir
-          :jar-file  jar-file})
-  (println (format "jar file created at: %s" jar-file)))
+(defn jar [{:keys [version] :or {version timestamp} :as params}]
+  (let [jar-file (jar-filename version)]
+    (clean nil)
+    (b/copy-dir {:src-dirs   ["src"]
+                 :target-dir target-dir})
+    (b/compile-clj {:basis     basis
+                    :src-dirs  ["src"]
+                    :class-dir class-dir})
+    (b/write-pom {:class-dir class-dir
+                  :lib       lib-name
+                  :version   version
+                  :basis     basis
+                  :src-dirs  ["src"]})
+    (b/jar {:class-dir class-dir
+            :jar-file  jar-file})
+    (println (format "jar file created at: %s" jar-file)))
+  params)
 
-(defn uberjar [_]
-  (clean nil)
-  (b/copy-dir {:src-dirs   ["src"]
-               :target-dir class-dir})
-  (b/compile-clj {:basis     basis
-                  :src-dirs  ["src"]
-                  :class-dir class-dir})
-  (b/uber {:class-dir class-dir
-           :uber-file uber-file
-           :basis     basis})
-  (println (format "uberjar file created at: %s" uber-file)))
+(defn uberjar [{:keys [version] :or {version timestamp} :as params}]
+  (let [uber-file (uber-filename version)]
+    (clean nil)
+    (b/copy-dir {:src-dirs   ["src"]
+                 :target-dir class-dir})
+    (b/compile-clj {:basis     basis
+                    :src-dirs  ["src"]
+                    :class-dir class-dir})
+    (b/uber {:class-dir class-dir
+             :uber-file uber-file
+             :basis     basis})
+    (println (format "uberjar file created at: %s" uber-file)))
+  params)
--- fudo-clojure.nix
+++ fudo-clojure.nix
@@ -3,23 +3,25 @@
 
 let
   base-name = "fudo-clojure";
+  version = "0.1";
+  full-name = "${base-name}-${version}";
 
   cljdeps = callPackage ./deps.nix { };
 
 in stdenv.mkDerivation {
-  name = base-name;
-  src = gitignoreSource ./.;
+  name = full-name;
+  src = ./.;
   buildInputs = [ clojure ];
   propagatedBuildInputs = map (d: d.paths) cljdeps.packages;
   buildPhase = ''
     HOME=$TEMP/home
     mkdir -p $HOME
-    clojure -X:build jar
+    clojure -X:build jar :version ${version}
   '';
   installPhase = ''
     mkdir -p $lib/share
     mkdir -p $out/share
-    cp ./target/${base-name}*.jar $lib/share
-    cp ./target/${base-name}*.jar $out/share
+    cp ./target/${base-name}-${version}.jar $lib/share
+    cp ./target/${base-name}-${version}.jar $out/share
   '';
 }
