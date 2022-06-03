1312a2805616b265ed4c89ef6216f300a7a7a155
niten <niten@fudo.org>
2022-06-02 19:47:48 -0700

    switching to clj-nix

--- flake.nix
+++ flake.nix
@@ -3,26 +3,37 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
-    clj2nix.url = "github:hlolli/clj2nix";
+    utils.url = "github:numtide/flake-utils";
+    clj-nix = {
+      url = "github:jlesquembre/clj-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, clj2nix, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      clj2nix-pkg = clj2nix.packages.${system}.clj2nix;
-      update-deps = pkgs.writeShellScriptBin "update-deps.sh" ''
-        ${clj2nix-pkg}/bin/clj2nix ./deps.edn ./deps.nix
-      '';
-    in {
-      packages."${system}".fudo-clojure =
-        pkgs.callPackage ./fudo-clojure.nix { };
-      defaultPackage."${system}" = self.packages."${system}".fudo-clojure;
-      overlay = final: prev: {
-        inherit (self.packages.${system}) fudo-clojure;
-      };
-      devShell."${system}" = pkgs.mkShell {
-        buildInputs = with pkgs; [ clojure clj2nix-pkg update-deps ];
-      };
-    };
+  outputs = { self, nixpkgs, utils, clj-nix, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        cljpkgs = clj-nix.packages."${system}";
+        update-deps = pkgs.writeShellScriptBin "update-deps.sh" ''
+          ${clj-nix.packages."${system}".deps-lock}/bin/deps-lock
+        '';
+      in {
+        packages = {
+          fudo-clojure = cljpkgs.mkCljBin {
+            projectSrc = ./.;
+            name = "org.fudo/fudo-clojure";
+            main-ns = "fudo-clojure.core";
+            jdkRunner = pkgs.jdk17_headless;
+
+            doCheck = true;
+            checkPhase = "clj -X:test";
+          };
+        };
+
+        defaultPackage = self.packages."${system}".fudo-clojure;
+
+        devShell =
+          pkgs.mkShell { buildInputs = with pkgs; [ clojure update-deps ]; };
+      });
 }
