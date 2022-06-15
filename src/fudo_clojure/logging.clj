982b80a2df747f52312c1251338bca6b7fadc30e
niten <niten@fudo.org>
2022-06-15 10:58:21 -0700

    [LOG] added log-to-function.
    Easily define new loggers that will apply default log-level logic.
    
    Also redefined print-logger in terms of log-to-function.

--- src/fudo_clojure/logging.clj
+++ src/fudo_clojure/logging.clj
@@ -25,14 +25,27 @@
     (fatal!  [_ _] nil)
 
     (info!   [_ _] nil)
-    (notify! [_ _] nil)))
+    (notify! [_ _] nil)
+    (alert!  [_ _] nil)))
 
-(defn print-logger []
-  (reify Logger
-    (debug!  [_ msg] (println msg))
-    (warn!   [_ msg] (println msg))
-    (error!  [_ msg] (println msg))
-    (fatal!  [_ msg] (println msg))
+(def error-log-level [:debug :warn :error :fatal])
+(def logic-log-level [:info :notify :alert])
 
-    (info!   [_ msg] (println msg))
-    (notify! [_ msg] (println msg))))
+(defn log-to-function
+  ([f] (log-to-function f :error :notify))
+  ([f error logic]
+   (let [error-level (.indexOf error-log-level error)
+         logic-level (.IndexOf logic-log-level logic)]
+     (reify Logger
+       (debug!  [_ msg] (when (>= error-level 0) (f msg)))
+       (warn!   [_ msg] (when (>= error-level 1) (f msg)))
+       (error!  [_ msg] (when (>= error-level 2) (f msg)))
+       (fatal!  [_ msg] (when (>= error-level 3) (f msg)))
+
+       (info!   [_ msg] (when (>= logic-level 0) (f msg)))
+       (notify! [_ msg] (when (>= logic-level 1) (f msg)))
+       (alert!  [_ msg] (when (>= logic-level 2) (f msg)))))))
+
+(defn print-logger
+  ([]            (print-logger :error :notify))
+  ([error logic] (log-to-function println error logic)))
