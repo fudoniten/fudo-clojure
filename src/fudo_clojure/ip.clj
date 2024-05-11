dd12f63229d6ba22d37270f6ecca7d1396e93c65
niten <niten@fudo.org>
2024-05-11 09:50:42 -0700

    Significant changes to ip implementation

--- src/fudo_clojure/ip.clj
+++ src/fudo_clojure/ip.clj
@@ -1,32 +1,102 @@
 (ns fudo-clojure.ip
-  (:require [fudo-clojure.net :as net])
+  (:require [fudo-clojure.net :as net]
+            [clojure.string :as str]
+            [clojure.math :as math])
   (:import com.google.common.net.InetAddresses
-           (java.net Inet4Address Inet6Address NetworkInterface)))
+           (java.net Inet4Address Inet6Address NetworkInterface InetAddress)))
 
-(defn- ipv4-impl? [ip] (instance? Inet4Address ip))
-(defn- ipv6-impl? [ip] (instance? Inet6Address ip))
+(defn- is-ipv4? [ip] (instance? Inet4Address ip))
+(defn- is-ipv6? [ip] (instance? Inet6Address ip))
+
+(defn- ip->int [ip]
+  (->> (InetAddresses/forString ip)
+       (.getAddress)
+       (reverse)
+       (zipmap (range))
+       (map (fn [[i n]] (.shiftLeft (biginteger n) (* i 8))))
+       (apply +)))
+
+(defn- int->ipv6 [n]
+  (InetAddresses/toAddrString
+   (Inet6Address/getByAddress "" (.toByteArray (biginteger n)))))
+
+(defn- int->ipv4 [n]
+  (InetAddresses/toAddrString
+   (Inet4Address/getByAddress "" (.toByteArray (biginteger n)))))
+
+(defn- ipv4->int [ip]
+  (->> (str/split ip #"\.")
+       (map #(Integer/parseInt %))
+       (reverse)
+       (zipmap (range))
+       (map (fn [[i n]] (bit-shift-left n (* i 8))))
+       (apply bit-or)))
+
+(defn- network-max-ip [net]
+  (let [[ip mask-str] (str/split net #"/")
+        mask          (Integer/parseInt mask-str)
+        total-bytes   (if (is-ipv4? (InetAddresses/forString ip)) 32 128)]
+    (.or (.subtract (biginteger (math/pow 2 (- total-bytes mask)))
+                    (biginteger 1))
+         (biginteger (ip->int ip)))))
+
+(defn- network-min-ip [net]
+  (let [[ip mask-str] (str/split net #"/")
+        mask          (Integer/parseInt mask-str)
+        total-bytes   (if (is-ipv4? (InetAddresses/forString ip)) 32 128)
+        shift         (- total-bytes mask)]
+    (-> (ip->int ip)
+        (biginteger)
+        (.shiftRight shift)
+        (.shiftLeft shift))))
+
+(defn- network-v4-min-ip [net]
+  (let [[ip mask-str] (str/split net #"/")
+        mask          (Integer/parseInt mask-str)
+        shift         (- 32 mask)]
+    (-> ip
+        (ipv4->int)
+        (bit-shift-left shift)
+        (unsigned-bit-shift-right shift)
+        (int->ipv4))))
+
+(defn- network-v4-range [net]
+  [(network-v4-min-ip net)
+   (network-v4-max-ip net)])
+
+(defn- network-range [subnet]
+  [(network-min-ip subnet) (network-max-ip subnet)])
+
+(defn- ip-on-subnet? [ip subnet]
+  (let [[min max] (network-range subnet)]
+    (<= min (ip->int ip) max)))
 
 (defn- is-tailscale-ip? [ip]
-  (net/ip-on-subnet? "100.64.0.0/10" (str ip)))
+  (or (ip-on-subnet? (str ip) "100.64.0.0/10")
+      (ip-on-subnet? (str ip) "fd7a:115c:a1e0::/96")))
 
-(defn- public-impl? [ip]
+(defn- ip-public? [ip]
   (and (not (.isLinkLocalAddress ip))
        (not (.isLoopbackAddress ip))
        (not (.isSiteLocalAddress ip))
        (not (is-tailscale-ip? (InetAddresses/toAddrString ip)))))
 
 (defprotocol IIPAddr
-  (ipv4?    [_])
-  (ipv6?    [_])
-  (public?  [_])
-  (private? [_]))
+  (ipv4?      [_])
+  (ipv6?      [_])
+  (public?    [_])
+  (private?   [_])
+  (tailscale? [_])
+  (on-subnet? [_ subnet]))
 
 (defrecord IPAddr [ip]
   IIPAddr
-  (ipv4?    [_] (ipv4-impl? ip))
-  (ipv6?    [_] (ipv6-impl? ip))
-  (public?  [_] (public-impl? ip))
-  (private? [_] (not (public? ip)))
+  (ipv4?      [_]        (is-ipv4? ip))
+  (ipv6?      [_]        (is-ipv6? ip))
+  (public?    [_]        (ip-public? ip))
+  (private?   [self]     (not (public? self)))
+  (tailscale? [_]        (is-tailscale-ip? (InetAddresses/toAddrString ip)))
+  (on-subnet? [_ subnet] (ip-on-subnet? ip subnet))
 
   Object
   (toString [_] (InetAddresses/toAddrString ip)))
@@ -37,8 +107,10 @@
 (defn from-int [i]
   (->IPAddr (InetAddresses/fromInteger i)))
 
+(defn- from-addr [addr] (->IPAddr addr))
+
 (defn get-host-ips []
   (->> (NetworkInterface/getNetworkInterfaces)
        (enumeration-seq)
        (mapcat #(enumeration-seq (.getInetAddresses %)))
-       (map #(IPAddr. %))))
+       (map from-addr)))
